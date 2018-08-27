e7c56a48d4f650bafe42a3bcdf8d1bb1a560fbe8
Nathan Cerny <ncerny@gmail.com>
2018-08-27 08:08:15 -0500

    change ports array to integer
    Signed-off-by: Nathan Cerny <ncerny@gmail.com>

--- attributes/20-acceptance.rb
+++ attributes/20-acceptance.rb
@@ -5,7 +5,7 @@ if node['domain'].eql?('acceptance.cerny.cc')
   default['glb']['forwarding_table']['hash_key'] = "93b9024bb9e610f8b933a5cd472d3e2f"
   default['glb']['forwarding_table']['seed'] = "242d94bc7ed12e940c6b7cc59fca76e7"
   default['glb']['forwarding_table']['binds'] = %w( 192.168.254.20/32 )
-  default['glb']['forwarding_table']['ports'] = %w( 80 443 8006 32400 )
+  default['glb']['forwarding_table']['ports'] = [ 80, 443, 8006, 32400 ]
   default['glb']['forwarding_table']['backends']['192.168.120.7'] = 'active'
   default['glb']['forwarding_table']['backends']['192.168.120.8'] = 'active'
   default['glb']['forwarding_table']['backends']['192.168.120.9'] = 'active'
